1e398cad71b67a662737edf1bfe736fe1d9bef66
Leandra Sousa <leandra.rayy@gmail.com>
2023-10-10 12:40:01 -0300

    feat: Implementada rota de cadastro do usuário e suas validações.

---
+++ src/midlewares/validateRequestBody.js
@@ -0,0 +1,10 @@
+const validateRequestBody = joiSchema => async (request, response, next) => {
+    try {
+        await joiSchema.validateAsync(request.body);
+        next();
+    } catch (error) {
+        return response.status(500).json(error.message);
+    }
+}
+
+module.exports = validateRequestBody
