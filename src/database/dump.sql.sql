a92ac9770919d48bf19c2b30a68399f76d9a0405
NanneSantin <elaine.s.santin@gmail.com>
2023-10-10 10:28:49 -0300

    feat: Criado o arquivo dump.sql

---
+++ src/database/dump.sql.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS "usuarios";
+DROP TABLE IF EXISTS "categorias";
+
+CREATE TABLE "usuarios"(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "nome" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "senha" TEXT NOT NULL
+);
+
+CREATE TABLE "categorias"(
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "categorias" VARCHAR(255) NOT NULL
+);
