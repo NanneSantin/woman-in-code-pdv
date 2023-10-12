d5d4145d1959508e6068b25fc13bdd1f12a25cc2
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-10-12 13:28:44 -0300

    feat: Rota Get, para listar categorias

--- src/router.js
+++ src/router.js
@@ -5,8 +5,11 @@ const loginSchema = require('./validations/loginSchema');
 const userSchema = require('./validations/userSchema');
 const authenticateUser = require('./controllers/login');
 const { validateAuthentication } = require('./middlewares/auth');
+const listCategories = require('./controllers/categories');
 
 const route = express();
+
+route.get('/categoria', listCategories);
 
 route.post('/usuario', validateRequestBody(userSchema), registerUser);
 route.post('/login', validateRequestBody(loginSchema), authenticateUser);
