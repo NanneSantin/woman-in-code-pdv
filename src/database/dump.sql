068ef0531b1bab0eff662a1fe1c7daeda13817d5
NanneSantin <elaine.s.santin@gmail.com>
2023-10-18 17:24:23 -0300

    feat: Adicionado tabelas produtos e clientes no dump.sql

--- src/database/dump.sql
+++ src/database/dump.sql
@@ -1,16 +1,18 @@
-DROP TABLE IF EXISTS "usuarios";
-DROP TABLE IF EXISTS "categorias";
+DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS categorias;
+DROP TABLE IF EXISTS produtos;
+DROP TABLE IF EXISTS clientes;
 
-CREATE TABLE "usuarios"(
-    "id" SERIAL PRIMARY KEY NOT NULL,
-    "nome" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "senha" TEXT NOT NULL
+CREATE TABLE usuarios(
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha TEXT NOT NULL
 );
 
-CREATE TABLE "categorias"(
-    "id" SERIAL PRIMARY KEY NOT NULL,
-    "categoria" VARCHAR(255) NOT NULL
+CREATE TABLE categorias(
+    id SERIAL PRIMARY KEY NOT NULL,
+    categoria VARCHAR(255) NOT NULL
 );
 
 INSERT INTO categorias (categoria) VALUES
@@ -23,3 +25,25 @@ INSERT INTO categorias (categoria) VALUES
     ('Moda'),
     ('Bebê'),
     ('Games');
+
+CREATE TABLE produtos(
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  descricao TEXT UNIQUE NOT NULL,
+  quantidade_estoque INT NOT NULL DEFAULT 0,
+  valor INT NOT NULL,
+  categoria_id INT NOT NULL REFERENCES categorias(id)
+);
+
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  cpf INT NOT NULL UNIQUE,
+  cep INT,
+  rua TEXT,
+  numero INT,
+  bairro VARCHAR(255),
+  cidade VARCHAR(255),
+  estado CHAR(2)
+);
+
