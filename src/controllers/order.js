b3569a2fa8b982ae1c45e1d16f1d9fafbc4b5269
Melissa Pereira <melissa2pereira@gmail.com>
2023-10-26 08:45:44 -0300

    criação da rota listar pedidos

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,68 +1,155 @@
-const knex = require('../connection');
-const send = require('../services/nodemailer');
-const compilerHtml = require('../utils/compiler');
+const knex = require("../connection");
+const send = require("../services/nodemailer");
+const compilerHtml = require("../utils/compiler");
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
 
 const registerOrder = async (request, response) => {
-    try {
-        const { cliente_id, observacao, pedido_produtos } = request.body;
+  try {
+    const { cliente_id, observacao, pedido_produtos } = request.body;
 
-        const insertOrder = await knex('pedidos').insert({ cliente_id, observacao, valor_total: 0 }).returning('*');
+    const insertOrder = await knex("pedidos")
+      .insert({ cliente_id, observacao, valor_total: 0 })
+      .returning("*");
 
-        const mapping = pedido_produtos.map(async (product) => {
-            const productCost = await knex('produtos').where('id', product.produto_id).select('valor').first();
+    const mapping = pedido_produtos.map(async (product) => {
+      const productCost = await knex("produtos")
+        .where("id", product.produto_id)
+        .select("valor")
+        .first();
 
-            await knex('pedido_produtos').insert({ pedido_id: insertOrder[0].id, produto_id: product.produto_id, quantidade_produto: product.quantidade_produto, valor_produto: productCost.valor }).returning('*');
-
-            return { valor: productCost.valor, id: product.produto_id }
-        });
-
-        const results = await Promise.all(mapping);
-
-        let totalCost = 0;
-        pedido_produtos.forEach(product => {
-            const found = results.find(result => product.produto_id === result.id);
-            totalCost = found ? totalCost + (found.valor * product.quantidade_produto) : totalCost;
-        });
-
-        await knex('pedidos').where('id', insertOrder[0].id).update('valor_total', totalCost);
-
-        pedido_produtos.forEach(async product => {
-            const stock = await knex('produtos').where('id', product.produto_id).select('quantidade_estoque').first();
-            const updatedStock = stock.quantidade_estoque - product.quantidade_produto;
-            await knex('produtos').where('id', product.produto_id).update('quantidade_estoque', updatedStock);
-        });
-
-        const client = await knex('clientes').where('id', cliente_id).first();
-
-        const products = await knex.select('pedidos.id', 'produtos.descricao', 'pedido_produtos.quantidade_produto', 'produtos.valor')
-            .from('pedido_produtos')
-            .innerJoin('produtos', 'pedido_produtos.produto_id', 'produtos.id')
-            .innerJoin('pedidos', 'pedido_produtos.pedido_id', 'pedidos.id')
-            .groupBy('pedidos.id', 'produtos.descricao', 'pedido_produtos.quantidade_produto', 'produtos.valor')
-            .where({ pedido_id: insertOrder[0].id });
-
-        const newListProducts = products.map(product => ({
-            produto: product.descricao,
-            quantidade: product.quantidade_produto,
-            valor_unitario: `R$ ${product.valor}`,
-            total: `R$ ${product.quantidade_produto * product.valor}`
-        }));
-
-        const html = await compilerHtml('./src/template/email.html', {
-            userName: client.nome,
-            id: insertOrder[0].id,
-            pedido_produtos: newListProducts,
-            valor_total: `R$ ${totalCost}`
+      await knex("pedido_produtos")
+        .insert({
+          pedido_id: insertOrder[0].id,
+          produto_id: product.produto_id,
+          quantidade_produto: product.quantidade_produto,
+          valor_produto: productCost.valor,
         })
+        .returning("*");
 
-        send(client.email, `Status do Pedido ${insertOrder[0].id}`, html)
+      return { valor: productCost.valor, id: product.produto_id };
+    });
 
-        return response.status(201).send();
-    } catch (error) {
-        return response.status(500).json({ message: 'Erro interno do servidor.' });
+    const results = await Promise.all(mapping);
+
+    let totalCost = 0;
+    pedido_produtos.forEach((product) => {
+      const found = results.find((result) => product.produto_id === result.id);
+      totalCost = found
+        ? totalCost + found.valor * product.quantidade_produto
+        : totalCost;
+    });
+
+    await knex("pedidos")
+      .where("id", insertOrder[0].id)
+      .update("valor_total", totalCost);
+
+    pedido_produtos.forEach(async (product) => {
+      const stock = await knex("produtos")
+        .where("id", product.produto_id)
+        .select("quantidade_estoque")
+        .first();
+      const updatedStock =
+        stock.quantidade_estoque - product.quantidade_produto;
+      await knex("produtos")
+        .where("id", product.produto_id)
+        .update("quantidade_estoque", updatedStock);
+    });
+
+    const client = await knex("clientes").where("id", cliente_id).first();
+
+    const products = await knex
+      .select(
+        "pedidos.id",
+        "produtos.descricao",
+        "pedido_produtos.quantidade_produto",
+        "produtos.valor"
+      )
+      .from("pedido_produtos")
+      .innerJoin("produtos", "pedido_produtos.produto_id", "produtos.id")
+      .innerJoin("pedidos", "pedido_produtos.pedido_id", "pedidos.id")
+      .groupBy(
+        "pedidos.id",
+        "produtos.descricao",
+        "pedido_produtos.quantidade_produto",
+        "produtos.valor"
+      )
+      .where({ pedido_id: insertOrder[0].id });
+
+    const newListProducts = products.map((product) => ({
+      produto: product.descricao,
+      quantidade: product.quantidade_produto,
+      valor_unitario: `R$ ${product.valor}`,
+      total: `R$ ${product.quantidade_produto * product.valor}`,
+    }));
+
+    const html = await compilerHtml("./src/template/email.html", {
+      userName: client.nome,
+      id: insertOrder[0].id,
+      pedido_produtos: newListProducts,
+      valor_total: `R$ ${totalCost}`,
+    });
+
+    send(client.email, `Status do Pedido ${insertOrder[0].id}`, html);
+
+    return response.status(201).send();
+  } catch (error) {
+    return response.status(500).json({ message: "Erro interno do servidor." });
+  }
+};
+
+const listOrders = async (request, response) => {
+  try {
+    const { cliente_id } = request.query;
+    const ordersList = [];
+
+    if (cliente_id) {
+      query = knex("pedidos")
+        .select("id as pedido_id", "valor_total", "observacao", "cliente_id")
+        .where("cliente_id", request.query.cliente_id);
+    } else {
+      query = knex("pedidos").select(
+        "id as pedido_id",
+        "valor_total",
+        "observacao",
+        "cliente_id"
+      );
     }
-}
+
+    const orders = await query;
+
+    for (const order of orders) {
+      const products = await knex("pedido_produtos")
+        .where("pedido_id", order.pedido_id)
+        .select(
+          "id as produto_id",
+          "quantidade_produto",
+          "valor_produto",
+          "pedido_id",
+          "produto_id"
+        );
+
+      ordersList.push({
+        pedido: {
+          id: order.pedido_id,
+          valor_total: order.valor_total,
+          observacao: order.observacao,
+          cliente_id: order.cliente_id,
+        },
+        pedido_produtos: products,
+      });
+    }
+
+    return response.status(200).json(ordersList);
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ message: "Erro interno do servidor." });
+  }
+};
 
 module.exports = {
-    registerOrder
-}
+  registerOrder,
+  listOrders,
+};
--- src/router.js
+++ src/router.js
@@ -1,65 +1,94 @@
-const express = require('express');
+const express = require("express");
 
-const { registerUser, detailUser, updateUser } = require('./controllers/users');
-const authenticateUser = require('./controllers/login');
-const listCategories = require('./controllers/categories');
+const { registerUser, detailUser, updateUser } = require("./controllers/users");
+const authenticateUser = require("./controllers/login");
+const listCategories = require("./controllers/categories");
 
 const {
   registerProduct,
   updateProduct,
   removeProduct,
   detailProduct,
-  listProducts
-} = require('./controllers/products');
+  listProducts,
+} = require("./controllers/products");
 
 const {
   registerCustomer,
   updateCustomer,
   listCustomers,
-  detailCustomer
-} = require('./controllers/customer');
+  detailCustomer,
+} = require("./controllers/customer");
 
-const validateRequestBody = require('./middlewares/validateRequestBody');
-const validateAuthentication = require('./middlewares/auth');
-const validateCategoryExist = require('./middlewares/validateCategoryExist');
-const validateCustomerExist = require('./middlewares/validateCustomerExist');
-const { validateProductIdExist, validateProductsOrder, validateProductsStock, checkIfProductIsInOrders } = require('./middlewares/validateProduct');
+const validateRequestBody = require("./middlewares/validateRequestBody");
+const validateAuthentication = require("./middlewares/auth");
+const validateCategoryExist = require("./middlewares/validateCategoryExist");
+const validateCustomerExist = require("./middlewares/validateCustomerExist");
+const {
+  validateProductIdExist,
+  validateProductsOrder,
+  validateProductsStock,
+  checkIfProductIsInOrders,
+} = require("./middlewares/validateProduct");
 
-const loginSchema = require('./validations/loginSchema');
-const userSchema = require('./validations/userSchema');
-const productSchema = require('./validations/productSchema');
-const customerSchema = require('./validations/customerSchema');
-const { registerOrder } = require('./controllers/order');
-const orderSchema = require('./validations/orderSchema');
+const loginSchema = require("./validations/loginSchema");
+const userSchema = require("./validations/userSchema");
+const productSchema = require("./validations/productSchema");
+const customerSchema = require("./validations/customerSchema");
+const { registerOrder, listOrders } = require("./controllers/order");
+const orderSchema = require("./validations/orderSchema");
 
 const route = express();
 
-route.post('/usuario', validateRequestBody(userSchema), registerUser);
-route.post('/login', validateRequestBody(loginSchema), authenticateUser);
-route.get('/categoria', listCategories);
+route.post("/usuario", validateRequestBody(userSchema), registerUser);
+route.post("/login", validateRequestBody(loginSchema), authenticateUser);
+route.get("/categoria", listCategories);
 
 route.use(validateAuthentication);
 
-route.get('/usuario', detailUser);
-route.put('/usuario', validateRequestBody(userSchema), updateUser);
+route.get("/usuario", detailUser);
+route.put("/usuario", validateRequestBody(userSchema), updateUser);
 
-route.post('/produto', validateRequestBody(productSchema), validateCategoryExist, registerProduct);
+route.post(
+  "/produto",
+  validateRequestBody(productSchema),
+  validateCategoryExist,
+  registerProduct
+);
 route.put(
-  '/produto/:id',
+  "/produto/:id",
   validateRequestBody(productSchema),
   validateProductIdExist,
   validateCategoryExist,
   updateProduct
 );
-route.delete('/produto/:id', validateProductIdExist, checkIfProductIsInOrders, removeProduct);
-route.get('/produto/:id', validateProductIdExist, detailProduct);
-route.get('/produto', validateCategoryExist, listProducts);
+route.delete(
+  "/produto/:id",
+  validateProductIdExist,
+  checkIfProductIsInOrders,
+  removeProduct
+);
+route.get("/produto/:id", validateProductIdExist, detailProduct);
+route.get("/produto", validateCategoryExist, listProducts);
 
-route.post('/cliente', validateRequestBody(customerSchema), registerCustomer);
-route.put('/cliente/:id', validateRequestBody(customerSchema), validateCustomerExist, updateCustomer);
-route.get('/cliente', listCustomers);
-route.get('/cliente/:id', validateCustomerExist, detailCustomer);
+route.post("/cliente", validateRequestBody(customerSchema), registerCustomer);
+route.put(
+  "/cliente/:id",
+  validateRequestBody(customerSchema),
+  validateCustomerExist,
+  updateCustomer
+);
+route.get("/cliente", listCustomers);
+route.get("/cliente/:id", validateCustomerExist, detailCustomer);
 
-route.post('/pedido', validateRequestBody(orderSchema), validateCustomerExist, validateProductsOrder, validateProductsStock, registerOrder);
+route.post(
+  "/pedido",
+  validateRequestBody(orderSchema),
+  validateCustomerExist,
+  validateProductsOrder,
+  validateProductsStock,
+  registerOrder
+);
+
+route.get("/pedido", listOrders);
 
 module.exports = route;
